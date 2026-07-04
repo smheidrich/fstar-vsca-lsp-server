4d70ff278a32b7d4124a075ba6e2caecafef0558
Shahriar Heidrich <smheidrich@weltenfunktion.de>
2026-07-05 01:19:34 +0200

    Use prepare lifecycle script instead of install
    GitOrigin-RevId: be6a5fd69db51bc6496accc96706f99e1d7cee09

--- make-package.sh
+++ make-package.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Modify the upstream package for publication
 #
-# The code is pretty easy to understand so I won't explain further here, you'll
-# figure it out.
-#
 # Requires:
 # - [jq](https://github.com/jqlang/jq)
+# - GNU coreutils compatible cp and sed (unlikely on e.g. Mac)
+
 set -euo pipefail
 
 mkdir -p build
+
 # Supposedly -T only works on Linux, not Mac OS. Too bad.
 cp -Tr upstream/fstar-vscode-assistant/lspserver/ build/fstar-vsca-lsp-server
 
@@ -16,22 +16,25 @@ cd build/fstar-vsca-lsp-server
 
 shopt -s globstar nullglob
 for x in src/**/*.{ts,js}; do
-  # As we use stdio as the LSP transport, we must prevent any other outputs to
-  # stdout; stderr is fine though:
-  sed -i 's/console.log/console.error/g' "$x"
+  # Because we use stdio as the LSP transport, we must prevent any other
+  # outputs to stdout; stderr is fine though:
+  sed -i -E 's/console.(log|info)/console.error/g' "$x"
 done
 
-# We need to change the package name, add a postinstall script to perform the
-# compilation step on the user's machine and then expose the compiled script
-# as an executable (jq lacks in-place editing support, hence the `jq ; mv`
-# thing):
+# We need to change the package name, add a prepare script to perform the
+# compilation with dev dependencies installed and then expose the compiled
+# script as an executable (jq lacks in-place editing support, hence the `jq ;
+# mv` thing):
 jq -f ../../package-json-filter.jq < package.json > package.json.tmp
 mv package.json.tmp package.json
 
-# We need to ensure the main script has a shebang:
-{ echo "#!/usr/bin/env node" ; cat src/main.ts; } > src/main.ts.tmp
-mv src/main.ts.tmp src/main.ts
+# We need to ensure the main script has a shebang (esbuild then automatically
+# sets the executable flag [^1]):
+sed -i '1i #!/usr/bin/env node' src/main.ts
+# [^1]: https://github.com/evanw/esbuild/commit/b808c0d905551dde55e2485eb18d488b12e4ba94
 
-# And we need to add `esbuild` to our dependencies so the postinstall script
-# can work:
-npm add 'esbuild@^0.28'
+# And we need to add `esbuild` to our devDependencies (upstream has it in the
+# parent package and invokes the lspserver compilation via the parent package's
+# tasks, which I guess under certain circumstances ensures devDependencies of
+# the parent are available, but we don't have that luxury):
+npm add --save-dev 'esbuild@^0.28'
--- package-json-filter.jq
+++ package-json-filter.jq
@@ -1,4 +1,3 @@
 .name |= "fstar-vsca-lsp-server" |
-.scripts.postinstall |= "npm run compile" |
-#.scripts.compile += " --external:vscode-* --external:which --external:ps-tree --external:node:* --banner:js=#!/usr/bin/env node" |
+.scripts.prepare |= "npm run compile" |
 .bin |= {"fstar-vsca-lsp-server": "out/main.js"}
